b71fd0cb1c6f301d60b8c137ef7c64b59bde1320
dianamunguia16 <dianamunguia023@gmail.com>
2021-06-14 13:58:46 -0600

    TAREAS Y PRACTICAS CAP 2 AL 5

--- src/tareasemana11del6/usodelwhile.java
+++ src/tareasemana11del6/usodelwhile.java
@@ -10,5 +10,21 @@ package tareasemana11del6;
  * @author diana
  */
 public class usodelwhile {
+public static void main(String[] args) { 
+    int i =0;
     
+    while(i <3){
+        
+        int j = 0;
+        while(j<3){
+            
+            System.out.println("A");
+            j++;   
+        }
+        } System.out.println();
+           i++;
+   }
+     
 }
+            
+
