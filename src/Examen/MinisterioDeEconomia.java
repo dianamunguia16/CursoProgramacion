b020952f39a58d6977d17e179736403c1f67e491
dianamunguia16 <dianamunguia023@gmail.com>
2021-07-02 18:39:47 -0600

    EXAMEN

--- src/Examen/MinisterioDeEconomia.java
+++ src/Examen/MinisterioDeEconomia.java
@@ -21,37 +21,95 @@ public class MinisterioDeEconomia {
         boolean canttrabajcovid = true;
         double ingresopromedio = 0;
         boolean creditos = true;
-
+        
         Scanner economi = new Scanner(System.in);
-
-        System.out.println("Seleccione la opcion de su interes\n1-Ingrese su informacion\2-Conozca su informacion");
+        
+        int contatrabco=0;
+        double poblacion=0;
+        System.out.println("Seleccione la opcion de su interes\n1-Ingrese su informacion\2-Conozca su informacion\n"
+                + "3-cantidad de personas por provincia");
         int opc = economi.nextInt();
 
         switch (opc) {
 
             case 1:
-                Scanner nombr = new Scanner(System.in);
+                
                 System.out.println("Ingrese el nombre completo de su empresa");
-                nombre = nombr.nextLine();
-                Scanner prov = new Scanner(System.in);
+                nombre = economi.nextLine();
                 System.out.println("Ingrese el nombre completo de la provincia donde se ubican sus trabajadores");
-                provincia= nombr.nextLine();
+                provincia= economi.nextLine();
                 Scanner canttrabajador = new Scanner(System.in);
                 System.out.println("Ingrese la cantidad de trabajadores que han tenido covid 19");
-                canttrabajcovid = nombr.nextBoolean();
-                Scanner ingreso = new Scanner(System.in);
+                canttrabajcovid = economi.nextBoolean();
+                if(canttrabajcovid)
+                    contatrabco++; // ESTO ES PARA CUENTE LOS TRABAJADORES CON COVID
+ 
                 System.out.println("Ingrese el monto ingreso promedio por mes");
-                ingresopromedio= ingreso.nextDouble();
-                Scanner credi = new Scanner(System.in);
+                ingresopromedio= economi.nextDouble();
                 System.out.println("Su empresa cuenta con creditos actualmente");
-                creditos = credi.nextBoolean();
+                creditos = economi.nextBoolean();
                 
                  break;
                  
                  
             case 2:
                  System.out.println("Sus datos corresponden a:"+nombre+provincia+ canttrabajcovid+ingresopromedio+creditos);
+                 if(contatrabco==20)
+                     System.out.println("ALERTA ESTO PODRIA EXPONER LA SALUD DE SUS TRABAJADORES");
+                     
+                 break;
+                 
+            case 3:
+                provincias(provincia,poblacion);
+                break;//LLAMO A LAS PROVINCIAS
+                 
+            case 4:
+                double limitcredit=0&&
+                
+                
+                
+                
         }
+        
+        
+        
 
     }
+    //PRIMER METODO QUE APLICO
+    public static void provincias(String provincia,double poblacion){
+    
+                if(provincia=="Alajuela"){
+                    poblacion =848146;
+                
+                }
+                else if (provincia=="San Jose"){
+                    poblacion = 1404242;
+                }
+                
+                else if (provincia=="Cartago"){
+                    poblacion = 490903;
+                }
+                else if (provincia=="Heredia"){
+                    poblacion = 433677;
+                }
+                else if (provincia=="Guanacaste"){
+                    poblacion = 326953;
+                }
+                else if (provincia=="Puntarenas"){
+                    poblacion = 410929;
+                }
+                 else if (provincia=="Limon"){
+                    poblacion = 386862;
+                }
+                 else{ 
+                 System.out.println("EL DATO SELECCIONADO ES INVALIDO");
+                 }
+    
+    
+    }
+    
 }
+
+
+
+
